70550b2fc38f2a1614d35cbf3eaec8d5529d529c
Mason from NoNames <57270034+masonruns@users.noreply.github.com>
2021-01-18 13:32:26 -0600

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const client = new Discord.Client();
-
-client.on('message', msg => {
-    const guildTag = msg.channel.type === 'text' ? `[${msg.guild.name}]` : '[DM]';
-    const channelTag = msg.channel.type === 'text' ? `[#${msg.channel.name}]` : '';
-    console.log(`${guildTag}${channelTag} ${msg.author.tag}: ${msg.content}`);
-});
-
-client.login('NzM3NDkwOTg1MTE4MDcyODYz.Xx-IAA.DdAD9eFitQ2IF98EggHWAB9VK2I');
